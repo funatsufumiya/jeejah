2ab46c4e090764bd1629a918df0532c1fe5401a7
Phil Hagelberg <phil@hagelb.org>
2016-06-07 19:26:42 +0700

    Change print and io.write to send over nrepl.

--- jeejah.lua
+++ jeejah.lua
@@ -7,45 +7,13 @@ local timeout = 0.1
 
 local pack = function(...) return {...} end
 local d = function(_) end
+local sessions = {}
 
 local serpent_opts = {maxlevel=8,maxnum=64,nocode=true}
-
-local eval = function(sandbox, code)
-   -- TODO: redirect stdin/stdout
-   local chunk, err = loadstring("return " .. code, "*socket*")
-   if(err and not chunk) then -- statement, not expression
-      chunk, err = loadstring(code, "*socket*")
-      if(not chunk) then
-         return false, "Compilation error: " .. (err or "unknown error")
-      end
-   end
-   if(sandbox) then setfenv(chunk, sandbox) end
-
-   local trace
-   local result = pack(xpcall(chunk, function(e)
-                                 trace = debug.traceback()
-                                 err = e end))
-   if(result[1]) then
-      local res, i = serpent.block(result[2], serpent_opts), 3
-      while i <= #result do
-         res = res .. ', ' .. serpent.block(result[i], serpent_opts)
-         i = i + 1
-      end
-      return res
-   else
-      return false, (err or "Unknown error") .. "\n" .. trace
-   end
-end
 
 local response_for = function(old_msg, msg)
    msg.session, msg.id, msg.ns = old_msg.session, old_msg.id, ""
    return msg
-end
-
--- TODO: proper session support?
-local register_session = function(msg)
-   local session = tostring(math.random(999999999))
-   return response_for(msg, {["new-session"]=session, status="done"})
 end
 
 local send = function(conn, msg)
@@ -59,18 +27,82 @@ local write_for = function(conn, msg)
    end
 end
 
+local print_for = function(write)
+   return function(...)
+      for _,x in ipairs({...}) do write(x) end
+      write("\n")
+   end
+end
+
+local sandbox_for = function(write, provided_sandbox)
+   local sandbox = { io = { write = write },
+                     print = print_for(write), }
+   for k,v in pairs(provided_sandbox) do
+      sandbox[k] = v
+   end
+   return sandbox
+end
+
+local eval = function(write, sandbox, code)
+   -- TODO: redirect stdin
+   local old_write, old_print = io.write, print
+   local chunk, err = loadstring("return " .. code, "*socket*")
+   if(err and not chunk) then -- statement, not expression
+      chunk, err = loadstring(code, "*socket*")
+      if(not chunk) then
+         return false, "Compilation error: " .. (err or "unknown error")
+      end
+   end
+   if(sandbox) then
+      setfenv(chunk, sandbox)
+   else
+      _G.print = print_for(write)
+      _G.io.write = write
+   end
+
+   local trace
+   local result = pack(xpcall(chunk, function(e)
+                                 trace = debug.traceback()
+                                 err = e end))
+   if(not sandbox) then
+      _G.print = old_print
+      _G.io.write = old_write
+   end
+
+   if(result[1]) then
+      local res, i = serpent.block(result[2], serpent_opts), 3
+      while i <= #result do
+         res = res .. ', ' .. serpent.block(result[i], serpent_opts)
+         i = i + 1
+      end
+      return res
+   else
+      return false, (err or "Unknown error") .. "\n" .. trace
+   end
+end
+
+-- TODO: proper session support?
+local register_session = function(msg)
+   local session = tostring(math.random(999999999))
+   sessions[session] = {}
+   return response_for(msg, {["new-session"]=session, status="done"})
+end
+
 -- see https://github.com/clojure/tools.nrepl/blob/master/doc/ops.md
-local handle = function(conn, handlers, sandbox, msg)
+local handle = function(conn, handlers, provided_sandbox, msg)
    if(msg.op == "clone") then
       d("New session.")
       send(conn, register_session(msg))
    elseif(msg.op == "eval") then
       d("Evaluating", msg.code)
-      local value, err = eval(sandbox, msg.code)
+      local write = write_for(conn, msg)
+      local sandbox = provided_sandbox and sandbox_for(write, provided_sandbox)
+      local value, err = eval(write, sandbox, msg.code)
       d("Got", value, err)
       send(conn, response_for(msg, {value=value, ex=err}))
       send(conn, response_for(msg, {status="done"}))
-   elseif(msg.op == "load-file" and not sandbox) then
+   elseif(msg.op == "load-file" and not provided_sandbox) then
+      -- TODO: sandbox this
       d("Loading file", msg.file)
       local ok, res = pcall(function() dofile(msg.file, msg["file-name"]) end)
       if(ok) then
@@ -89,7 +121,7 @@ local handle = function(conn, handlers, sandbox, msg)
       write_for(conn, msg)("Describe is not supported.\n")
    elseif(handlers[msg.op]) then
       d("Custom op:", msg.op)
-      handlers[msg.op](conn, msg, sandbox)
+      handlers[msg.op](conn, msg, provided_sandbox)
    else
       send(conn, response_for(msg, {status="unknown-op"}))
       print("  | Unknown op", serpent.block(msg))
