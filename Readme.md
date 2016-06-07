eacd3cd623e1e6589d647ce27c2f806323f5b7a9
Phil Hagelberg <phil@hagelb.org>
2016-06-07 19:17:45 +0700

    Initial commit.

---
+++ Readme.md
@@ -0,0 +1,46 @@
+# JeeJah
+
+An nREPL server for Lua.
+
+## Installation
+
+    $ luarocks install --local jeejah
+
+Make sure `~/.luarocks/bin` is on your `$PATH`.
+
+Or from source:
+
+    $ luarocks install --local luasocket
+    $ luarocks install --local serpent
+    $ luarocks install --local bencode
+
+You can symlink `bin/jeejah` to your `$PATH` or something.
+
+## Usage
+
+    $ jeejah
+
+Accepts `--host` and `--port` args. Also accepts `--debug` flag.
+
+Currently only tested with [monroe](https://github.com/sanel/monroe/)
+as a client. Install and run `M-x monroe`.
+
+You can use it as a library too, of course:
+
+```lua
+local jeejah = require("jeejah")
+jeejah(host, port, {debug=true, fg=true, sandbox={x=12}})
+```
+
+If you don't set `fg=true` then you will get back a coroutine which
+you'll need to repeatedly resume in order to handle requests.
+
+You can also pass in a `handlers` table where the keys are custom
+[nREPL ops](https://github.com/clojure/tools.nrepl/blob/master/doc/ops.md)
+you want to handle yourself.
+
+## License
+
+Copyright © 2016 Phil Hagelberg and contributors
+
+Distributed under the MIT license; see file LICENSE
---
+++ jeejah.lua
@@ -0,0 +1,168 @@
+-- these first few should all be in luarocks
+local socket = require "socket"
+local serpent = require "serpent"
+local bencode = require "bencode"
+
+local timeout = 0.1
+
+local pack = function(...) return {...} end
+local d = function(_) end
+
+local serpent_opts = {maxlevel=8,maxnum=64,nocode=true}
+
+local eval = function(sandbox, code)
+   -- TODO: redirect stdin/stdout
+   local chunk, err = loadstring("return " .. code, "*socket*")
+   if(err and not chunk) then -- statement, not expression
+      chunk, err = loadstring(code, "*socket*")
+      if(not chunk) then
+         return false, "Compilation error: " .. (err or "unknown error")
+      end
+   end
+   if(sandbox) then setfenv(chunk, sandbox) end
+
+   local trace
+   local result = pack(xpcall(chunk, function(e)
+                                 trace = debug.traceback()
+                                 err = e end))
+   if(result[1]) then
+      local res, i = serpent.block(result[2], serpent_opts), 3
+      while i <= #result do
+         res = res .. ', ' .. serpent.block(result[i], serpent_opts)
+         i = i + 1
+      end
+      return res
+   else
+      return false, (err or "Unknown error") .. "\n" .. trace
+   end
+end
+
+local response_for = function(old_msg, msg)
+   msg.session, msg.id, msg.ns = old_msg.session, old_msg.id, ""
+   return msg
+end
+
+-- TODO: proper session support?
+local register_session = function(msg)
+   local session = tostring(math.random(999999999))
+   return response_for(msg, {["new-session"]=session, status="done"})
+end
+
+local send = function(conn, msg)
+   d("Sending", bencode.encode(msg))
+   conn:send(bencode.encode(msg))
+end
+
+local write_for = function(conn, msg)
+   return function(...)
+      send(conn, response_for(msg, {out=table.concat({...}, "\t")}))
+   end
+end
+
+-- see https://github.com/clojure/tools.nrepl/blob/master/doc/ops.md
+local handle = function(conn, handlers, sandbox, msg)
+   if(msg.op == "clone") then
+      d("New session.")
+      send(conn, register_session(msg))
+   elseif(msg.op == "eval") then
+      d("Evaluating", msg.code)
+      local value, err = eval(sandbox, msg.code)
+      d("Got", value, err)
+      send(conn, response_for(msg, {value=value, ex=err}))
+      send(conn, response_for(msg, {status="done"}))
+   elseif(msg.op == "load-file" and not sandbox) then
+      d("Loading file", msg.file)
+      local ok, res = pcall(function() dofile(msg.file, msg["file-name"]) end)
+      if(ok) then
+         send(conn, response_for(msg, {status="done", value=res}))
+      else
+         send(conn, response_for(msg, {status="done", ex=res}))
+      end
+   elseif(msg.op == "stdin") then
+      d("Stdin", serpent.block(msg))
+      return -- TODO: implement
+   elseif(msg.op == "interrupt") then
+      d("Interrupt")
+      return -- we can't do anything to interrupt, ignore silently
+   elseif(msg.op == "describe") then
+      d("Describe")
+      write_for(conn, msg)("Describe is not supported.\n")
+   elseif(handlers[msg.op]) then
+      d("Custom op:", msg.op)
+      handlers[msg.op](conn, msg, sandbox)
+   else
+      send(conn, response_for(msg, {status="unknown-op"}))
+      print("  | Unknown op", serpent.block(msg))
+   end
+end
+
+local function receive(conn, yield, partial)
+   local s, err = conn:receive(1) -- wow this is primitive
+   yield()
+   if(s) then
+      return receive(conn, yield, (partial or "") .. s)
+   elseif(err == "timeout" and partial == nil) then
+      return receive(conn, yield)
+   elseif(err == "timeout") then
+      return partial
+   else
+      return nil, err
+   end
+end
+
+local function loop(server, handlers, sandbox, yield)
+   local conn, err = server:accept()
+   yield()
+   if(conn) then
+      conn:settimeout(timeout)
+      d("Connected.")
+      while true do
+         local input, r_err = receive(conn, yield)
+         if(input) then
+            local decoded, d_err = bencode.decode(input)
+            yield()
+            if(decoded) then
+               handle(conn, handlers, sandbox, decoded)
+            else
+               print("  | Decoding error:", d_err)
+            end
+         else
+            if(r_err == "closed") then
+               return loop(server, handlers, sandbox, yield)
+            elseif(r_err ~= "timeout") then
+               print("  | Error:", r_err)
+            end
+         end
+      end
+   else
+      if(err ~= "timeout") then print("  | Socket error: " .. err) end
+      return loop(server, handlers, sandbox, yield)
+   end
+end
+
+-- Start an nrepl socket server on the given host and port. For opts
+-- you can pass a table with fg=true to run in the foreground, debug=true for
+-- verbose logging, and sandbox={...} to evaluate all code in a sandbox.
+-- You can also give an opts.handlers table keying ops to handler functions
+-- which take the socket, the decoded message, and the optional sandbox table.
+return function(host, port, opts)
+   host, port = host or "localhost", port or 7888
+   local server, err = assert(socket.bind(host, port))
+   opts = opts or {}
+   if(opts.debug) then d = print end
+   if(opts.timeout) then timeout = tonumber(opts.timeout) end
+
+   if(server) then
+      server:settimeout(timeout)
+      print("Server started on " .. host .. ":" .. port .. "...")
+      if(opts.fg) then
+         return loop(server, opts.sandbox, opts.handlers, function() end)
+      else
+         return coroutine.create(function()
+               loop(server, opts.sandbox, opts.handlers, coroutine.yield)
+         end)
+      end
+   else
+      print("  | Error starting socket repl server: " .. err)
+   end
+end
