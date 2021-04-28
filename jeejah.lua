bef15e4e7132e68aadcece6903e61bea6387368a
Grazfather <grazfather@gmail.com>
2021-04-28 13:57:43 -0400

    Take 'serializer' in opt

--- jeejah.lua
+++ jeejah.lua
@@ -6,9 +6,7 @@ local load = loadstring or load
 local timeout = 0.001
 
 local d = os.getenv("DEBUG") and print or function(_) end
-local pprinter = function(p) return function(x)
-	p(x) end
-end
+local serializer = tostring
 local sessions = {}
 
 local response_for = function(old_msg, msg)
@@ -65,11 +63,11 @@ local execute_chunk = function(session, chunk, pp)
    local old_write, old_print, old_read = io.write, print, io.read
    if(session.sandbox) then
       setfenv(chunk, session.sandbox)
-      pp = pp or pprinter(session.sandbox.print)
+      pp = pp or session.sandbox.print
    else
       _G.print = print_for(session.write)
       _G.io.write, _G.io.read = session.write, session.read
-      pp = pp or pprinter(_G.print)
+      pp = pp or _G.print
    end
 
    local trace, err
@@ -206,13 +204,13 @@ local handle = function(conn, handlers, sandbox, msg)
       local session_sandbox = session_for(conn, msg, sandbox).sandbox
       send(conn, complete(msg, session_sandbox))
    elseif(msg.op == "stdin") then
-      d("Stdin", pp(msg))
+      d("Stdin", serializer(msg))
       sessions[msg.session].input = msg.stdin
       send(conn, response_for(msg, {status={"done"}}))
       return
    elseif(msg.op ~= "interrupt") then -- silently ignore interrupt
       send(conn, response_for(msg, {status={"unknown-op"}}))
-      print("  | Unknown op", pp(msg))
+      print("  | Unknown op", serializer(msg))
    end
 end
 
@@ -322,17 +320,16 @@ return {
       -- host should always be localhost on a PC, but not always on a micro
       local server = assert(socket.bind(opts.host or "localhost", port))
       if(opts.debug) then d = print end
-      if(opts.pprinter) then
-	      if(opts.pprinter == "serpent") then
-		      serpent = require("serpent")
-		      local serpent_pp = function(p) return function(x)
-			      local serpent_opts = {maxlevel=8,maxnum=64,nocode=true}
-			      p(serpent.block(x, serpent_opts)) end
-            end
-         else
-            print("Unsupported pretty printer")
-         end
-		end
+      if(opts.serialize) then
+         serializer = opts.serialize
+      else
+          local serpent = require("serpent")
+          local serpent_pp = function(x)
+              local serpent_opts = {maxlevel=8,maxnum=64,nocode=true}
+              serpent.block(x, serpent_opts)
+          end
+          serializer = serpent_pp
+      end
       if(opts.timeout) then timeout = tonumber(opts.timeout) end
       if(opts.fennel) then
          local fenneleval = require("jeejah.fenneleval")
